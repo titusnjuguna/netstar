84f3efc2cb478ea39a9b2dcba849a6ad488ef94c
Tito <titusnjuguna59@gmail.com>
2026-06-24 10:35:30 +0300

    Chore:fix model error

--- api/models/setup.py
+++ api/models/setup.py
@@ -16,10 +16,11 @@ class RouterInfo(Base):
     hotspot_name = Column(String)
     till_number = Column(String)
     hostname = Column(String)
-    created_at = Column(DateTime,default=datetime.utcnow)
     reg_token = Column(String, unique=True, nullable=True)
     public_ip = Column(String, nullable=True)   # IP the VPS sees the router coming from
     last_seen = Column(DateTime, nullable=True)  # last time the router called home
+
+
 class Products(Base):
     __tablename__= "products"
     id = Column(Integer,primary_key=True,index=True)
