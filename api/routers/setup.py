f3d78a69eac8f0e1c6efddfdee3378103b2e5eff
Tito <titusnjuguna59@gmail.com>
2026-07-02 15:38:52 +0300

    Chore:add delete

--- api/routers/setup.py
+++ api/routers/setup.py
@@ -99,7 +99,7 @@ def create_router(routerInfo: RouterCreate, _: dict = Depends(verify_token)):
 #exclude from auth verification since this is called by the router script
 
 @router.post("/hotspot/pay/{router_id}", response_model=GeneralResponse)
-def hotspot_pay(router_id: int, payload: HotspotPayRequest, db: Session = Depends(get_db)):
+def hotspot_pay(router_id: str, payload: HotspotPayRequest, db: Session = Depends(get_db)):
     db_router = db.query(RouterInfo).filter(RouterInfo.reg_token== router_id).first()
     if not db_router:
         return GeneralResponse(message="Router not found", success=False, code=404)
