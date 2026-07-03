23cf537c8015deef5eba008d047248e6bef9d1ae
Tito <titusnjuguna59@gmail.com>
2026-07-03 10:35:14 +0300

    Chore:add delete

--- api/services/payment.py
+++ api/services/payment.py
@@ -41,23 +41,24 @@ def stk_push_request(amount,phone,till_number,product_id,db):
     passkey= os.getenv("MPESA_PASSKEY")
     password = f"{till_number}{passkey}{timestamp}"
     encoded_credentials = base64.b64encode(password.encode('utf-8')).decode('utf-8')
-    headers = {'Content-Type': 'application/json','Authorization': f"'Bearer {token}'"}
+    headers = {'Content-Type': 'application/json', 'Authorization': f"Bearer {token}"}
     till_number = 174379
-    payload = {    
-        "BusinessShortCode": till_number,    
-        "Password": encoded_credentials,    
-        "Timestamp": timestamp,    
-        "TransactionType": "CustomerPayBillOnline",    
-        "Amount": amount,    
-        "PartyA":phone,    
-        "PartyB": till_number,    
-        "PhoneNumber": phone,    
-        "CallBackURL": os.getenv("CALLBACK_URL"),    
-        "AccountReference": f'Session:{session_ref}',    
-        "TransactionDesc":f"Payment for hotspot {phone}"
-        }
+    payload = {
+        "BusinessShortCode": till_number,
+        "Password": encoded_credentials,
+        "Timestamp": timestamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": amount,
+        "PartyA": phone,
+        "PartyB": till_number,
+        "PhoneNumber": phone,
+        "CallBackURL": os.getenv("CALLBACK_URL"),
+        "AccountReference": f'Session:{session_ref}',
+        "TransactionDesc": f"Payment for hotspot {phone}"
+    }
     stk_push_url = os.getenv("STK_PUSH_URL")
-    response = requests.request("POST",stk_push_url,headers = headers, data = payload)
+    print(f"STK push request payload: {payload}")
+    response = requests.request("POST", stk_push_url, headers=headers, json=payload)
     if response.json().get("ResponseCode") == "0":
         print(f"STK push request sent. Response: {response.text}")
         #add the payment record to the database
@@ -84,9 +85,5 @@ def stk_push_request(amount,phone,till_number,product_id,db):
     else:
         print(f"Error occurred while making STK push request: {response.text}")
         return {"error": "Failed to initiate STK push request", "details": response.text, "status_code": response.status_code}
-
-
-
-
 
 # {"Success":"True","Code":200,"message":"Success","customer":"Tito","limit":"1000","balance":0,"access":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM3MzYwMDgwLCJqdGkiOiJhNzQ5NTQ4MTU4Njg0N2VjYjE5ZmRlYzUzN2U5OWUzNyIsInVzZXJfaWQiOjF9.XsuRk7Vi17QZUpcxO4YPi_mifkywQ1-HZlHkUQgFDlE","refresh":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTczNzQzNTY4MCwianRpIjoiOWIyZGU3YmU5MDQ4NDJiOWE4ZWI3NzI4NTRjY2ZiMDMiLCJ1c2VyX2lkIjoxfQ.kuL_xQ0JYWi50b2UmKbLVPVJYb9VYNsDah07qa44nSs"}
