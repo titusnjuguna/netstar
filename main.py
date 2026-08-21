72cb3ab593a032358d8ab3d101b74061fdd112c6
Tito <titusnjuguna59@gmail.com>
2026-08-22 01:48:51 +0300

    Fix file

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 
 template = Jinja2Templates(directory=os.path.join("api", "templates"))
-
+#test fixing
 
 app = FastAPI()
 
